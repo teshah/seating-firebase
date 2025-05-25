842cb2038c14928fbca3df57b0fc16326dab6995
Tejash Shah <tejashshah3@gmail.com>
2025-05-25 14:28:54 +0000

    show time only

--- src/components/birthday-countdown-timer.tsx
+++ src/components/birthday-countdown-timer.tsx
@@ -12,7 +12,7 @@ interface TimeLeft {
 // Target date: May 25, 2025, 7:00 PM ET.
 // ET is EDT (UTC-4) in May. So, 19:00 ET is 23:00 UTC.
 const TARGET_DATE_UTC = new Date(Date.UTC(2025, 4, 25, 23, 0, 0)); // Month 4 is May, 23 hours is 7 PM ET
-const FORMATTED_TARGET_DATE = "(05/25 07:00 PM)"; // Removed ET
+const FORMATTED_TARGET_DATE = "(07:00 PM)"; // Removed ET
 
 const calculateTimeLeft = (): TimeLeft | null => {
   const difference = +TARGET_DATE_UTC - +new Date(); // `+` converts date to number (milliseconds)
